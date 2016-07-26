6e5b3252bf204303b82ee3c98d1f9c5e1da8204d
Miika Havo <miika.havo@bittituki.fi>
2016-07-26 09:46:50 +0300

    MODIFIED: Changed FS -> VPK

--- application/classes/model/hto.php
+++ application/classes/model/hto.php
@@ -13,10 +13,9 @@ class Model_HTO extends Kohana_Model {
         ';
 
 		$data = $this->_db->query(Database::SELECT, $sql, FALSE)->as_array();
-        return $data; 
+        return $data;
      }
-     
-    
+
     /**
      * Returns a list of people signed up for the given date.
      *
@@ -30,42 +29,41 @@ class Model_HTO extends Kohana_Model {
 		if(!$date) {
 			$date = date('Y-m-d');
 		}
-	
+
 		$query = DB::query(Database::SELECT, '
-			SELECT * FROM date_person 
-			WHERE date = :date 
-			ORDER BY 
-				f_xdz_pilot DESC, 
-				f_ctq_pilot DESC, 
-				f_licenced DESC, 
-				f_s_aff DESC, 
-				f_s_aff2 DESC, 
-				f_s_sl DESC, 
+			SELECT * FROM date_person
+			WHERE date = :date
+			ORDER BY
+				f_xdz_pilot DESC,
+				f_ctq_pilot DESC,
+				f_licenced DESC,
+				f_s_aff DESC,
+				f_s_aff2 DESC,
+				f_s_sl DESC,
 				f_s_tandem DESC,
 				name
 		');
-	
+
 		$query->param(':date', $date);
-		
+
 		$data = $query->execute()->as_array();
-		
-		
+
 		// Go through the rows, and set attributes for the day
 		$dayCTQPilot = false;
 		$dayXDZPilot = false;
-		
+
 		$dayNoAFFInstructors = 0;
 		$dayAFFInstructors = '';
-		
+
 		$dayNoSLInstructors = 0;
 		$daySLInstructors = '';
 
 		$dayNoRadioInstructors = 0;
 		$dayRadioInstructors = '';
-		
+
 		$dayFSInstructor = false;
 		$dayTandemInstructor = false;
-		
+
 		foreach($data as $row) {
 			if($row['f_ctq_pilot']) {
 				$dayCTQPilot = $row['name'];
@@ -92,7 +90,7 @@ class Model_HTO extends Kohana_Model {
 				$dayTandemInstructor = $row['name'];
 			}
 		}
-		
+
 		// Clean up extra commas from instructor lists
 		$dayAFFInstructors = trim($dayAFFInstructors, ' /');
 		$daySLInstructors = trim($daySLInstructors, ' /');
@@ -125,7 +123,6 @@ class Model_HTO extends Kohana_Model {
 				$row['role'] = '?';
 			}
 			// done with role
-			
 
 			// Set happiness and notes based on available and needed resources
 			$happyNotes = '';
@@ -140,13 +137,13 @@ class Model_HTO extends Kohana_Model {
 				$row['happiness'] = 0;
 			}
 			else if(!$dayXDZPilot && !$dayCTQPilot) {
-				$row['happiness'] = 0;				
+				$row['happiness'] = 0;
 			}
-			
+
 			if($row['f_unhappy']) {
-				$row['happiness'] = 0;				
+				$row['happiness'] = 0;
 			}
-			
+
 			// maybe add later: (- FS S with I-B and no I-FS)
 			if($row['f_s_sl']) {
 				if($dayNoSLInstructors) {
@@ -157,7 +154,7 @@ class Model_HTO extends Kohana_Model {
 					$row['happiness'] = 0;
 				}
 			}
-			
+
 			if($row['f_s_aff']) {
 				if($dayAFFInstructors) {
 					$happyNotes .= ' Mesu: '.$dayAFFInstructors.', ';
@@ -181,23 +178,23 @@ class Model_HTO extends Kohana_Model {
 					$row['happiness'] = 0;
 				}
 			}
-			
+
 			if($row['f_s_fs']) {
 				if($dayFSInstructor) {
-					$happyNotes .= ' FS-kouluttaja: '.$dayFSInstructor.', ';
+					$happyNotes .= 'VPK: '.$dayFSInstructor.', ';
 				}
 				else {
-					$unhappyNotes .= ' Tarvitsee FS-kouluttajan, ';
+                    $unhappyNotes .= ' Tarvitsee VPK:n';
 					$row['happiness'] = 0;
 				}
 			}
-			
+
 			if($row['f_s_radio']) {
 				if($dayNoRadioInstructors) {
 					// An SL-student's instructor can't be on the radio for him
 					if(
 						$row['f_s_sl'] &&
-						$dayNoRadioInstructors == 1 && 
+						$dayNoRadioInstructors == 1 &&
 						$dayNoSLInstructors == 1 &&
 						$daySLInstructors == $dayRadioInstructors
 					) {
@@ -207,7 +204,7 @@ class Model_HTO extends Kohana_Model {
 					// An AFF-student's instructor can't be on the radio for him
 					else if(
 						$row['f_s_aff'] &&
-						$dayNoRadioInstructors == 1 && 
+						$dayNoRadioInstructors == 1 &&
 						$dayNoAFFInstructors == 1 &&
 						$dayAFFInstructors == $dayRadioInstructors
 					) {
@@ -217,7 +214,7 @@ class Model_HTO extends Kohana_Model {
 					// An AFF-student's instructors can't be on the radio for him
 					else if(
 						$row['f_s_aff2'] &&
-						$dayNoRadioInstructors == 2 && 
+						$dayNoRadioInstructors == 2 &&
 						$dayNoAFFInstructors == 2 &&
 						$dayAFFInstructors == $dayRadioInstructors
 					) {
@@ -226,7 +223,7 @@ class Model_HTO extends Kohana_Model {
 					}
 					else if(
 						$row['f_s_aff2'] &&
-						$dayNoRadioInstructors == 1 && 
+						$dayNoRadioInstructors == 1 &&
 						$dayNoAFFInstructors == 1 &&
 						$dayAFFInstructors == $dayRadioInstructors
 					) {
@@ -234,7 +231,7 @@ class Model_HTO extends Kohana_Model {
 						$row['happiness'] = 0;
 					}
 					else {
-						$happyNotes .= ' Radisti: '.$dayRadioInstructors.', ';					
+						$happyNotes .= ' Radisti: '.$dayRadioInstructors.', ';
 					}
 
 				}
@@ -243,7 +240,7 @@ class Model_HTO extends Kohana_Model {
 					$row['happiness'] = 0;
 				}
 			}
-			
+
 			if($row['f_s_tandem']) {
 				if($dayTandemInstructor) {
 					$happyNotes .= ' Tandem-mesu: '.$dayTandemInstructor.', ';
@@ -253,8 +250,7 @@ class Model_HTO extends Kohana_Model {
 					$row['happiness'] = 0;
 				}
 			}
-			
-			
+
 			// INSTRUCTOR flags
 			if($row['f_i_aff']) {
 				$happyNotes .= 'NOVA-mesu, ';
@@ -266,12 +262,11 @@ class Model_HTO extends Kohana_Model {
 				$happyNotes .= 'radiokouluttaja, ';
 			}
 			if($row['f_i_fs']) {
-				$happyNotes .= 'FS-kouluttaja, ';
+				$happyNotes .= 'VPK';
 			}
 			if($row['f_i_tandem']) {
 				$happyNotes .= 'tandem-mesu, ';
 			}
-
 
 			// Plane flags
 			if($row['f_ctq_only']) {
@@ -290,14 +285,14 @@ class Model_HTO extends Kohana_Model {
 					$unhappyNotes .= ' Tarvitsee XDZ:n, ';
 				}
 			}
-			
+
 			$row['happyNotes'] = trim($happyNotes, ' ,');
 			$row['unhappyNotes'] = trim($unhappyNotes, ' ,');
-			
+
 			if($row['happyNotes'] && $row['unhappyNotes']) {
 				$row['happyNotes'] .= ", ";
 			}
-			
+
 			// Format the seconds off the timestamp, and don't show anything if there's no info
 			if($row['from_time'] == '00:00:00') {
 				$row['from_time'] = '';
@@ -306,22 +301,21 @@ class Model_HTO extends Kohana_Model {
 				$row['from_time'] = substr($row['from_time'], 0, 5);
 			}
 
-			
 			$newData[] = $row;
 		}
 
 		// Day-wide flags
 		$dayInfo = array(
 			'pilotMissing' => 0,
-			'canceled' => 0, 
+			'canceled' => 0,
 			'notes' => '',
 		);
-		
+
 		// Missing pilot?
 		if(!$dayCTQPilot && !$dayXDZPilot) {
 			$dayInfo['pilotMissing'] = 1;
 		}
-		
+
 		// Get info for day
 		$query = DB::query(Database::SELECT, 'SELECT * FROM date WHERE date = :date');
 		$query->param(':date', $date);
@@ -332,7 +326,7 @@ class Model_HTO extends Kohana_Model {
 			if($data[0]['canceled']) {
 				$dayInfo['canceled'] = 1;
 			}
-			
+
 			// Other info?
 			$dayInfo['notes'] = $data[0]['notes'];
 		}
@@ -343,10 +337,9 @@ class Model_HTO extends Kohana_Model {
 			'day' => $dayInfo,
 		);
 
-		return $finalData; 
+		return $finalData;
 	}
-	
-	
+
 	/**
 	 * @param $data Array The _unfiltered_ POST array from the AJAX call.
 	 *
@@ -355,7 +348,7 @@ class Model_HTO extends Kohana_Model {
 		$query = DB::query(Database::INSERT, '
 			INSERT INTO date_person(
 				name,
-				date, 
+				date,
 				from_time,
 				notes,
 				f_ctq_pilot,
@@ -378,7 +371,7 @@ class Model_HTO extends Kohana_Model {
 			)
 			values(
 				:name,
-				:date, 
+				:date,
 				:from_time,
 				:notes,
 				:f_ctq_pilot,
@@ -400,9 +393,9 @@ class Model_HTO extends Kohana_Model {
 				:f_s_tandem
 			)
 		');
-	
+
 		$this->_generateQuery($query, $data);
-		
+
 		if($query->execute()) {
 			return true;
 		}
@@ -410,17 +403,16 @@ class Model_HTO extends Kohana_Model {
 			return false;
 		}
 	}
-	
-	
+
 	/**
 	 * @param $data Array The _unfiltered_ POST array from the AJAX call.
 	 *
 	 */
 	public function editPerson($data) {
 		$query = DB::query(Database::UPDATE, '
-			UPDATE date_person SET 
+			UPDATE date_person SET
 				name = :name,
-				date = :date, 
+				date = :date,
 				from_time = :from_time,
 				notes = :notes,
 				f_ctq_pilot = :f_ctq_pilot,
@@ -444,9 +436,9 @@ class Model_HTO extends Kohana_Model {
 		');
 
 		$this->_generateQuery($query, $data);
-		
+
 		$query->param(':id', $data['hTOId']);
-		
+
 		if($query->execute()) {
 			return true;
 		}
@@ -454,8 +446,7 @@ class Model_HTO extends Kohana_Model {
 			return false;
 		}
 	}
-	
-	
+
 	/**
 	 * helper function that sets parameters for Add and Edit queries
 	 *
@@ -465,15 +456,14 @@ class Model_HTO extends Kohana_Model {
 		$fields = array(
 			'hTOPersonType',
 			'hTOStudentType',
-		);		
-		
+		);
+
 		foreach($fields as $field) {
 			if(!isset($data[$field])) {
 				$data[$field] = false;
 			}
 		}
-		
-		
+
 		// For checkbox fields the check is a bit more complex:
 		$checkBoxFields = array(
 			'hTO_f_i_aff',
@@ -481,21 +471,20 @@ class Model_HTO extends Kohana_Model {
 			'hTO_f_i_radio',
 			'hTO_f_i_fs',
 			'hTO_f_i_tandem',
-			
+
 			'hTO_f_s_radio',
 			'hTO_f_s_fs',
 		);
-		
+
 		foreach($checkBoxFields as $field) {
 			if(isset($data[$field]) && $data[$field] == 'on'){
 				$data[$field] = 1;
 			}
 			else {
-				$data[$field] = 0;			
+				$data[$field] = 0;
 			}
 		}
-		
-		
+
 		// And for the duplicate checkbox fields the check is even more complex.
 		// In addition to the check, we set key 'hTO_f_unhappy', which isn't set in the POST
 		// array.
@@ -504,19 +493,19 @@ class Model_HTO extends Kohana_Model {
 			'hTO_f_xdz_only',
 			'hTO_f_unhappy',
 		);
-		
+
 		foreach($checkBoxFields as $field) {
 			if(isset($data[$field.'_l']) && $data[$field.'_l'] == 'on'){
 				$data[$field] = 1;
 			}
 			else if(isset($data[$field.'_s']) && $data[$field.'_s'] == 'on') {
-				$data[$field] = 1;			
+				$data[$field] = 1;
 			}
 			else {
-				$data[$field] = 0;			
+				$data[$field] = 0;
 			}
 		}
-		
+
 		// The name just gets chopped to 30 chars
 		$name = substr(htmlspecialchars($data['hTOName']), 0, 30);
 		if(!$name) {
@@ -532,7 +521,7 @@ class Model_HTO extends Kohana_Model {
 		else {
 			// Try to make sure the string is interpreted correctly
 			if(strlen($fromTime) == 5) {
-				// Let's try fixing the timestamp. For example "12.00" 
+				// Let's try fixing the timestamp. For example "12.00"
 				// doesn't work, but "12:00" does.
 				$fromTime = substr_replace($fromTime, ':', 2, 1);
 			}
@@ -540,7 +529,7 @@ class Model_HTO extends Kohana_Model {
 				$fromTime = substr_replace($fromTime, ':', 1, 1);
 			}
 			else if(strlen($fromTime) == 3) {
-				// can't think of any sensible time format with three chars. Except "3pm", but 
+				// can't think of any sensible time format with three chars. Except "3pm", but
 				// we're just not that american.
 				$fromTime = '';
 			}
@@ -553,34 +542,34 @@ class Model_HTO extends Kohana_Model {
 			}
 		}
 		logg('final fromTime: '.$fromTime);
-		
+
 		$notes = substr(htmlspecialchars($data['hTONotes']), 0, 100);
 		if(!$notes) {
 			$notes = '';
 		}
-		
+
 		$query->param(':name', $name);
 		$query->param(':date' , $data['hTODate']);
 		$query->param(':from_time', $fromTime);
 		$query->param(':notes', $notes);
-		
+
 		switch($data['hTOPersonType']) {
 			case 'hTOPersonLicenced':
 				$query->param(':f_ctq_pilot', 0);
 				$query->param(':f_xdz_pilot', 0);
-				
+
 				$query->param(':f_ctq_only', $data['hTO_f_ctq_only']);
 				$query->param(':f_xdz_only', $data['hTO_f_xdz_only']);
-				
+
 				$query->param(':f_licenced', 1);
 				$query->param(':f_unhappy', $data['hTO_f_unhappy']);
-				
+
 				$query->param(':f_i_aff', $data['hTO_f_i_aff']);
 				$query->param(':f_i_sl', $data['hTO_f_i_sl']);
 				$query->param(':f_i_radio', $data['hTO_f_i_radio']);
 				$query->param(':f_i_fs', $data['hTO_f_i_fs']);
 				$query->param(':f_i_tandem', $data['hTO_f_i_tandem']);
-				
+
 				$query->param(':f_s_aff', 0);
 				$query->param(':f_s_aff2', 0);
 				$query->param(':f_s_sl', 0);
@@ -588,15 +577,15 @@ class Model_HTO extends Kohana_Model {
 				$query->param(':f_s_fs', 0);
 				$query->param(':f_s_tandem', 0);
 			break;
-			
+
 			case 'hTOPersonStudent':
-			
+
 				$query->param(':f_ctq_pilot', 0);
 				$query->param(':f_xdz_pilot', 0);
-				
+
 				$query->param(':f_ctq_only', $data['hTO_f_ctq_only']);
 				$query->param(':f_xdz_only', $data['hTO_f_xdz_only']);
-				
+
 				$query->param(':f_licenced', 0);
 				$query->param(':f_unhappy', $data['hTO_f_unhappy']);
 
@@ -605,20 +594,20 @@ class Model_HTO extends Kohana_Model {
 				$query->param(':f_i_radio', 0);
 				$query->param(':f_i_fs', 0);
 				$query->param(':f_i_tandem', 0);
-				
+
 				switch($data['hTOStudentType']) {
 					case 'hTO_f_s_sl':
 						$query->param(':f_s_aff', 0);
 						$query->param(':f_s_aff2', 0);
 						$query->param(':f_s_sl', 1);
 					break;
-					
+
 					case 'hTO_f_s_aff':
 						$query->param(':f_s_aff', 1);
 						$query->param(':f_s_aff2', 0);
 						$query->param(':f_s_sl', 0);
 					break;
-					
+
 					case 'hTO_f_s_aff2':
 						$query->param(':f_s_aff', 0);
 						$query->param(':f_s_aff2', 1);
@@ -629,16 +618,15 @@ class Model_HTO extends Kohana_Model {
 				$query->param(':f_s_radio', $data['hTO_f_s_radio']);
 				$query->param(':f_s_fs', $data['hTO_f_s_fs']);
 				$query->param(':f_s_tandem', 0);
-			
+
 			break;
-			
+
 			case 'hTOPersonTandemStudent':
 				$query->param(':f_ctq_pilot', 0);
 				$query->param(':f_xdz_pilot', 0);
 
 				$query->param(':f_ctq_only', $data['hTO_f_ctq_only']);
 				$query->param(':f_xdz_only', $data['hTO_f_xdz_only']);
-				
 
 				$query->param(':f_licenced', 0);
 				$query->param(':f_unhappy', 0);
@@ -654,7 +642,7 @@ class Model_HTO extends Kohana_Model {
 				$query->param(':f_s_fs', 0);
 				$query->param(':f_s_tandem', 1);
 			break;
-			
+
 			case 'hTOPersonCTQPilot':
 				$query->param(':f_ctq_pilot', 1);
 				$query->param(':f_xdz_pilot', 0);
@@ -674,7 +662,7 @@ class Model_HTO extends Kohana_Model {
 				$query->param(':f_s_fs', 0);
 				$query->param(':f_s_tandem', 0);
 			break;
-			
+
 			case 'hTOPersonXDZPilot':
 				$query->param(':f_ctq_pilot', 0);
 				$query->param(':f_xdz_pilot', 1);
@@ -695,10 +683,9 @@ class Model_HTO extends Kohana_Model {
 				$query->param(':f_s_tandem', 0);
 			break;
 		}
-		
+
 	}
-	
-	
+
 	/**
 	 * @param Integer $numberOfDates How many dates do we want to show? Default value is
 	 * used, except when getting the expanded calendar, in which case the number is selected
@@ -710,7 +697,7 @@ class Model_HTO extends Kohana_Model {
 		$dates = array();
 		for($i=0 ; $i < $numberOfDates ; $i++) {
 			$date['date'] = date('Y-m-d', $time);
-			
+
 			$fDate = date('j.n.', $time);
 			$weekday = substr(strftime('%A', $time), 0, 2);
 			$date['print'] = $weekday.'&nbsp;'.$fDate;
@@ -718,14 +705,14 @@ class Model_HTO extends Kohana_Model {
 
 			// Get summary for participation information
 			$dateData = $this->getPeopleForDate($date['date']);
-			
+
 			$people = $dateData['people'];
-			
+
 			$xDZPilots = 0;
 			$cWWPilots = 0;
 			$happyPeople = 0;
 			$unhappyPeople = 0;
-			
+
 			$summary = array(
 				'xdz' => array(
 					's' => array(
@@ -734,11 +721,11 @@ class Model_HTO extends Kohana_Model {
 					),
 					'l' => array(
 						'0' => 0,
-						'1' => 0,						
+						'1' => 0,
 					),
 					'pilot' => array(
 						'0' => 0,
-						'1' => 0,						
+						'1' => 0,
 					),
 				),
 				'ctq' => array(
@@ -748,15 +735,15 @@ class Model_HTO extends Kohana_Model {
 					),
 					'l' => array(
 						'0' => 0,
-						'1' => 0,						
+						'1' => 0,
 					),
 					'pilot' => array(
 						'0' => 0,
-						'1' => 0,						
-					),			
+						'1' => 0,
+					),
 				),
 			);
-			
+
 			foreach($people as $person) {
 				// This data is for generating the date background image:
 				if($person['role'] == 'CTQ-pilotti') {
@@ -771,7 +758,7 @@ class Model_HTO extends Kohana_Model {
 				else if($person['happiness'] == 0) {
 					$unhappyPeople++;
 				}
-				
+
 				// This data is for the summary:
 
 				if($person['f_licenced']) {
@@ -792,7 +779,7 @@ class Model_HTO extends Kohana_Model {
 					$key2 = 'l';
 				}
 				else {
-					$key2 = 's';				
+					$key2 = 's';
 				}
 				*/
 				if(!$person['f_ctq_only']) {
@@ -800,7 +787,7 @@ class Model_HTO extends Kohana_Model {
 					// Add to XDZ array
 					$summary['xdz'][$key2][$person['happiness']] = $summary['xdz'][$key2][$person['happiness']] + 1;
 				}
-				
+
 				if(!$person['f_xdz_only']) {
 					logg('adding to ctq');
 					// Add to CTQ array
@@ -810,25 +797,24 @@ class Model_HTO extends Kohana_Model {
 			}
 
 			$date['summary'] = $summary;
-			
+
 			$date['xDZPilots'] = $xDZPilots;
 			$date['cWWPilots'] = $cWWPilots;
 			$date['happyPeople'] = $happyPeople;
 			$date['unhappyPeople'] = $unhappyPeople;
-			
+
 			$date['canceled'] = $dateData['day']['canceled'];
 			$date['notes'] = $dateData['day']['notes'];
-			
+
 			$date['N'] = date('N', $time);
-			
+
 			$dates[] = $date;
 			$time = strtotime('+1 day', $time);
 		}
-	
+
 		return $dates;
 	}
-	
-	
+
 	/**
 	 * @param $id Row id
 	 *
@@ -837,18 +823,17 @@ class Model_HTO extends Kohana_Model {
 		$query = DB::query(Database::DELETE, '
 			DELETE FROM date_person WHERE id = :id
 		');
-		
+
 		$query->param(':id', $id);
-		
+
 		if($query->execute()) {
 			return true;
 		}
 		else {
 			return false;
 		}
-		
-	}
 
+	}
 
 	/**
 	 * Gets a single row from the main table, used for populating the edit form.
@@ -856,9 +841,9 @@ class Model_HTO extends Kohana_Model {
 	 */
 	public function getRow($id) {
 		$query = DB::query(Database::SELECT, 'SELECT * FROM date_person WHERE id = :id');
-	
+
 		$query->param(':id', $id);
-		
+
 		$data = $query->execute()->as_array();
 
 		// Drop the seconds, and the whole element if it's not set
@@ -866,19 +851,17 @@ class Model_HTO extends Kohana_Model {
 			$data[0]['from_time'] = '';
 		}
 		else {
-			$data[0]['from_time'] = substr($data[0]['from_time'], 0, 5);		
+			$data[0]['from_time'] = substr($data[0]['from_time'], 0, 5);
 		}
 
-
 		return $data[0];
-	}	
-
+	}
 
 	/**
 	 * @param $data Array The _unfiltered_ POST array from the AJAX call.
 	 *
 	 */
-	public function updateDate($data) {	
+	public function updateDate($data) {
 		$query = DB::query(Database::INSERT, '
 			INSERT INTO date(
 				date,
@@ -886,15 +869,15 @@ class Model_HTO extends Kohana_Model {
 				notes
 			)
 			values(
-				:date, 
+				:date,
 				:canceled,
 				:notes
 			)
-			ON DUPLICATE KEY UPDATE 
+			ON DUPLICATE KEY UPDATE
 				canceled = :canceled,
 				notes = :notes
 		');
-	
+
 		// Handle the checkbox:
 		if(isset($data['hTOEditDateCanceled']) && $data['hTOEditDateCanceled'] == 'on') {
 			$query->param(':canceled', 1);
@@ -902,19 +885,18 @@ class Model_HTO extends Kohana_Model {
 		else {
 			$query->param(':canceled', 0);
 		}
-		
+
 		// Handle notes:
-		$notes = substr(htmlspecialchars($data['hTOEditDateNotes']), 0, 100);	
+		$notes = substr(htmlspecialchars($data['hTOEditDateNotes']), 0, 100);
 		// Kohana will cast false into 0, but we want an empty string:
 		if(!$notes) {
 			$notes = '';
 		}
 		$query->param(':notes', $notes);
-		
+
 		// No need to do anything about the date:
 		$query->param(':date', $data['hTODate']);
-		
-		
+
 		if($query->execute()) {
 			return true;
 		}
@@ -925,16 +907,16 @@ class Model_HTO extends Kohana_Model {
 
 	function logg($thing) {
 		$fp = fopen('application/logs/debug.log', 'a');
-		
+
 		if(is_array($thing) || is_object($thing)) {
 			fputs($fp, print_r($thing, true));
 		}
 		else {
-			fputs($fp, $thing);	
+			fputs($fp, $thing);
 		}
-		
+
 		fputs($fp, "\n");
-		
+
 		fclose($fp);
 	}
 }
